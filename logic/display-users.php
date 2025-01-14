77355527626f50dad04a2c079cf365c6781fafee
Khardiata.Diop <khardiata.diop@outlook.com>
2025-01-14 16:24:48 +0000

    Afficher les User étape 2 : préparer les données

--- logic/display-users.php
+++ logic/display-users.php
@@ -1,2 +1,12 @@
 <?php
 
+    require __DIR__ . '/../managers/UserManager.class.php';
+    
+    $manager = new UserManager();
+    
+    $manager->loadUsers();
+    
+    $users = $manager->getUsers();
+    
+?>
+    
